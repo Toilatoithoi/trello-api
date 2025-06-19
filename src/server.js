d8ae1e9b1a9c29f752636a201f6c720f3539d3c6
duchoangkim123 <duc42165@huce.edu.vn>
2025-06-19 21:01:08 +0700

    init

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World Nodejs</h1>')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Hello, I'm running server at http://${hostname}:${port}/`)
+})
