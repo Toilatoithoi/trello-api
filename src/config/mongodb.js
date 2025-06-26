76f3769d0e880019dd4db17183f26d63d3062e0b
duchoangkim123 <duc42165@huce.edu.vn>
2025-06-26 17:25:26 +0700

    49. Tổ chức biến môi trường ENV đúng cách

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,17 +3,14 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
-
-const MONGODB_URI = 'mongodb+srv://hts_dev:yK8amlkKd9CvWRvW@cluster0.jwt8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const DATABASE_NAME = 'trello-mern-stack-pro'
-
 import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
 
 // Khởi tạo một đối tưởng trelloDatabaseInstance ban đầu là null (vì chúng ta chưa connect)
 let trelloDatabaseInstance = null
 
 // Khởi tạo một đối tượng Client Instance để connect tới MongoDB
-const mongoClinetInstance = new MongoClient(MONGODB_URI, {
+const mongoClinetInstance = new MongoClient(env.MONGODB_URI, {
   // Lưu ý: cái serverApi có từ phiên bản 5.0.0 trở lên, có thể không cần dùng đến nó, còn nếu dùng nó là chúng ta sẽ chị định một cái Stable API Version của MongoDB
   // Đọc thêm ở đây: https://www.mongodb.com/docs/drivers/node/current/fundamentals/stable-api/
   serverApi: {
@@ -29,7 +26,7 @@ export const CONNECT_DB = async() => {
   await mongoClinetInstance.connect()
 
   // Kết nối thành công thì lấy ra Database theo tên và gán ngược nó lại vào biến trelloDatabaseInstance ở trên của chúng ta
-  trelloDatabaseInstance = mongoClinetInstance.db(DATABASE_NAME)
+  trelloDatabaseInstance = mongoClinetInstance.db(env.DATABASE_NAME)
 }
 
 // Đóng kết nối tới Database khi cần
--- src/server.js
+++ src/server.js
@@ -7,27 +7,24 @@
 
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
 const START_SERVER = () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
-
   app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
     res.end('<h1>Hello World!</h1><hr>')
   })
 
-  app.listen(port, hostname, () => {
-    console.log(`3. Hi, Back-end Server is running successfully at Host: ${hostname} and Port: ${port}`)
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hi ${env.AUTHOR}, Back-end Server is running successfully at Host: ${env.APP_HOST} and Port: ${env.APP_PORT}`)
   })
 
   // Thực hiện các tác vụ cleanup trước khi dừng server
   // Đọc thêm ở đây: https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
   exitHook(() => {
-    console.log('4. Disconnecting from MongoDB Cloud Atlas...')
+    console.log('4. Server is shutting down...')
     CLOSE_DB()
     console.log('5. Disconnected from MongoDB Cloud Atlas')
   })
